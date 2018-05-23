e4fd7e29013a58e418faac86296d905251f113db
Sławomir Kaleta <slaszka@gmail.com>
2018-05-23 13:11:53 +0200

    prepare to unittest

---
+++ tests/Bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+session_start();
+include_once dirname(__FILE__) . '/../vendor/autoload.php';
+
+# Application configuration
+define('APP_DIR', dirname(__FILE__) . '/app/');
---
+++ tests/ConnectionTest.php
@@ -0,0 +1,48 @@
+<?php
+use PHPUnit_Framework_Constraint_IsType as PHPUnit_IsType;
+
+use Dframe\Database\Database;
+use Dframe\Database\Pdohelper;
+use PDO;
+
+
+//@Todo
+//@Todo
+
+class ConnectionTest extends PHPUnit_Framework_TestCase
+{
+
+    protected $backupGlobals = false;
+    private $conn = null;
+
+    public function getConnection()
+    {
+        if ($this->conn === null) {
+            try {
+                $dbConfig = array(
+                    'dbtype' => 'mysql',
+                    'host' => '',
+                    'dbname' => 'test',
+                    'username' => 'root',
+                    'password' => '',
+                );
+                $this->db = new Database($dbConfig);
+
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
+        return $this->db;
+    }
+
+    public function testEmptyPdoQuery()
+    {
+        try {
+            $test = $this->getConnection()->pdoQuery();
+        } catch(Exception $e) {
+            $this->assertEquals($e->getMessage(), 'SQLSTATE[42000]: Syntax error or access violation: 1065 Query was empty');
+        }
+
+    }
+
+}
