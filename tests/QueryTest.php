c5d7a4746d2fb312a5bec101bb327344ce40b770
Sławomir Kaleta <slaszka@gmail.com>
2018-05-23 18:23:41 +0200

    set namespace, add more phpunit

---
+++ tests/QueryTest.php
@@ -0,0 +1,55 @@
+<?php
+namespace Dframe\Database\tests;
+
+use PHPUnit_Framework_Constraint_IsType as PHPUnit_IsType;
+
+use Dframe\Database\Database;
+use Dframe\Database\Pdohelper;
+
+
+class QueryTest extends \Dframe\Database\tests\TestSetUp
+{
+    public function setUp()
+    {
+        $this->dataSetup = array(
+            'data1' => array(
+                'username' => 'Jack_' . uniqid()
+            ),
+            'data2' => array(
+                array('username' => 'Eli_' . uniqid()),
+                array('username' => 'Mat_' . uniqid()),
+                array('username' => 'Andre_' . uniqid())
+            )
+        );
+    }
+
+    public function testSelectUser()
+    {
+        $select = $this->getConnection()->select('users')->results();
+        $this->assertArrayHasKey('user_id', $select[0]);
+
+        // Collects all rows
+        $select = $this->getConnection()->select('users', '*')->result();
+        $this->assertArrayHasKey('user_id', $select);
+    }
+
+    public function testInsert()
+    {
+        $dataArray = array('username' => $this->dataSetup['data1']['username']);
+        $insert = $this->getConnection()->insert('users', $dataArray)->getLastInsertId();
+        $this->assertTrue(is_numeric($insert));
+    }
+
+    public function testInsertBatch()
+    {
+        $insert = $this->getConnection()->insertBatch('users', $this->dataSetup['data2'])->getAllLastInsertId();
+        $this->assertCount(3, $insert);
+    }
+
+    public function testUpdate(){
+        $dataArray = array('phone' => rand(100, 999).'-'. rand(100, 999).'-'. rand(100, 999));
+        $where = array('user_id' => rand(1, 1000));
+        $update = $this->getConnection()->update('users', $dataArray, $where)->affectedRows();
+        $this->assertTrue(is_numeric($update));
+    }
+}
---
+++ tests/TestSetUp.php
@@ -0,0 +1,33 @@
+<?php
+namespace Dframe\Database\tests;
+
+use Dframe\Database\Database;
+
+// backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase') and class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
+abstract class TestSetUp extends \PHPUnit\Framework\TestCase
+{
+
+    public function getConnection()
+    {
+        try {
+            $dbConfig = array(
+                'dbtype' => 'mysql',
+                'host' => 'localhost',
+                'dbname' => 'test',
+                'username' => 'root',
+                'password' => '',
+            );
+            $this->db = new Database($dbConfig);
+
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+        }
+
+        return $this->db;
+    }
+
+}
