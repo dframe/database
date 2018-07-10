bf667bcf9a2d0ae9178ed2e215bb2f4c572cd120
Sławomir Kaleta <slaszka@gmail.com>
2018-07-10 16:41:10 +0200

    Resolved #3, More flexible config

--- src/Database.php
+++ src/Database.php
@@ -1,6 +1,7 @@
 <?php
 namespace Dframe\Database;
 
+use PDO;
 use Dframe\Database\PdoWrapper;
 
 /**
@@ -29,6 +30,13 @@ class Database extends PdoWrapper
         if (is_null($this->config)) {
             $this->config = array(
                 'logDir' => APP_DIR . 'View/logs/',
+                'attributes' => array(
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", 
+                    //PDO::ATTR_ERRMODE => PDO::ERRMODE_SILENT,  // Set pdo error mode silent
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // If you want to Show Class exceptions on Screen, Uncomment below code 
+                    PDO::ATTR_EMULATE_PREPARES => true, // Use this setting to force PDO to either always emulate prepared statements (if TRUE), or to try to use native prepared statements (if FALSE). 
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // Set default pdo fetch mode as fetch assoc
+                )
             );
         }
 
