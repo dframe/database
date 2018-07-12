330d9ba56b71b66ea9e2b2978419356fb1a33bfd
Dusta <dusta@users.noreply.github.com>
2018-07-12 21:39:39 +0200

    Update tests

--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -9,42 +9,9 @@ use \PDO;
 //@Todo
 //@Todo
 
-class ConnectionTest extends \PHPUnit\Framework\TestCase
+class ConnectionTest extends TestSetUp
 {
-
-    private $db = null;
-
-    public function getConnection()
-    {
-
-        try {
-            $dbConfig = [
-                'dbtype' => 'mysql',
-                'host' => 'localhost',
-                'dbname' => 'test',
-                'username' => 'root',
-                'password' => '',
-            ];
-
-            $config = [
-                'logDir' => APP_DIR . 'View/logs/',
-                'attributes' => [
-                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // If you want to Show Class exceptions on Screen, Uncomment below code 
-                    PDO::ATTR_EMULATE_PREPARES => true, // Use this setting to force PDO to either always emulate prepared statements (if TRUE), or to try to use native prepared statements (if FALSE). 
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // Set default pdo fetch mode as fetch assoc
-                 ]
-            ];
-
-            $this->db = new Database($dbConfig, $config);
-
-        } catch (\PDOException $e) {
-            echo $e->getMessage();
-        }
-
-        return $this->db;
-    }
-
+    
     public function testEmptyPdoQuery()
     {
         $this->expectException(\Exception::class);
