fad9940fa42eca295edf4a8c98775c75d736984c
Dusta <dusta@users.noreply.github.com>
2018-07-12 21:39:37 +0200

    Update tests

--- tests/TestSetUp.php
+++ tests/TestSetUp.php
@@ -15,9 +15,20 @@ abstract class TestSetUp extends TestCase
                 'host' => 'localhost',
                 'dbname' => 'test',
                 'username' => 'root',
-                'password' => ''
+                'password' => '',
             ];
-            $this->db = new Database($dbConfig);
+
+            $config = [
+                'logDir' => APP_DIR . 'View/logs/',
+                'attributes' => [
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // If you want to Show Class exceptions on Screen, Uncomment below code 
+                    PDO::ATTR_EMULATE_PREPARES => true, // Use this setting to force PDO to either always emulate prepared statements (if TRUE), or to try to use native prepared statements (if FALSE). 
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // Set default pdo fetch mode as fetch assoc
+                 ]
+            ];
+
+            $this->db = new Database($dbConfig, $config);
 
         } catch (\PDOException $e) {
             echo $e->getMessage();
