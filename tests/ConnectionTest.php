d15851632ddf58bfdfb98138a403f7d4a14fea18
Dusta <dusta@users.noreply.github.com>
2018-09-28 14:28:17 +0200

    Init #13, PHPUnit - Failed asserting that exception of type

--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -10,7 +10,7 @@ class ConnectionTest extends TestSetUp
     public function testEmptyPdoQuery()
     {
         $this->expectException(\PDOException::class);
-        $this->expectExceptionMessage('SQLSTATE[42000]: Syntax error or access violation: 1065 Query was empty');
+        $this->expectExceptionMessage('Query is empty..');
 
         $test = $this->getConnection()->pdoQuery();
     }
